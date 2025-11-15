aa66c6f5f08f9f852f1ba568b11beab7683144bc
unknown <yasidalmubarok@gmail.com>
2025-11-15 16:09:47 +0700

    fix: update UserEntity struct to use time.Time for CreatedAt field

--- internal/entity/user_entity.go
+++ internal/entity/user_entity.go
@@ -1,8 +1,10 @@
 package entity
 
+import "time"
+
 type UserEntity struct {
-	ID      int `gorm:"primaryKey;autoIncrement"`
-	Name    string `gorm:"type:varchar(100);not null;"`
-	Hobbies string `gorm:"type:varchar(255);not null;"`
-	CreatedAt string `gorm:"type:timestamp;default:current_timestamp"`
+	ID        int    `gorm:"primaryKey;autoIncrement"`
+	Name      string `gorm:"type:varchar(100);not null;"`
+	Hobbies   string `gorm:"type:varchar(255);not null;"`
+	CreatedAt time.Time `gorm:"type:timestamp;default:current_timestamp"`
 }
